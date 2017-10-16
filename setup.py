fbb55f09ec9cd04e8a137f9ea890cfe2eb4bd58e
osechet <osechet@gmail.com>
2017-10-16 09:11:33 +0200

    Fix bld script definition in setup.py

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'bld=bldlib.bld:run',
+            'bld=bldlib.bld:main',
         ],
     },
 )
