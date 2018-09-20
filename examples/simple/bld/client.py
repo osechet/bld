d64f8423e4f1fe6ae072a764820032da396dea2b
Olivier Sechet <osechet@gmail.com>
2018-09-20 19:15:18 +0200

    Update example

--- examples/simple/bld/client.py
+++ examples/simple/bld/client.py
@@ -30,8 +30,9 @@ def build(project, args):
 
         with project.step('long_op', "Long operation"):
             run('echo "A very very very very long line that seams to never end"')
-            for i in range(1, 10):
-                run('echo "Doing something - %d"' % i)
-                time.sleep(0.5)
+            total = 100
+            for i in range(1, total):
+                run('echo "Doing something - %d/%d"' % (i, total))
+                time.sleep(0.025)
             run('echo "Another very very very very long line that seams to never end"')
             run('echo "Another very very very very long line that with a little bit of luck should be much much much bigger than the width of ther running terminal"')
